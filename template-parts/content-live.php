c36b28850b1880f5130a6bf9a639035db9d28a60
Austin Crane <austincrane@gmail.com>
2018-10-02 11:27:00 -0400

    fix iframe

--- template-parts/content-live.php
+++ template-parts/content-live.php
@@ -59,7 +59,7 @@
                     <style>.embed-container { position: relative; padding-bottom: 56.25%; padding-top: 30px; height: 0; overflow: hidden; max-width: 100%; height: auto; } .embed-container iframe, .embed-container object, .embed-container embed { position: absolute; top: 0; left: 0; width: 100%; height: 100%; }</style>
                     <div class='embed-container'>
                         <iframe id="playerIFrame" frameborder="0" scrolling="no" align="middle"
-                                src="http://player.piksel.com/playerlive.php?s=b2tj2evy&r=true&doResize=false"
+                                src="https://player.piksel.com/playerlive.php?s=b2tj2evy&r=true&doResize=false"
                                 height="360" width="640"
                                 allowtransparency="true" allowfullscreen>
                         </iframe>
@@ -74,7 +74,7 @@
                         <style>.embed-container-2 { position: relative; padding-bottom: 56.25%; padding-top: 30px; height: 0; overflow: hidden; max-width: 100%; height: 1000px; } .embed-container-2 iframe, .embed-container-2 object, .embed-container-2 embed { position: absolute; top: 0; left: 0; width: 100%; height: 100%; }</style>
                         <div class='embed-container-2'>
                                 <iframe frameborder="0" scrolling="no" align="middle"
-                                    src="http://player.piksel.com/player.php?p=dek7nez2&wmode=transparent&r=true"
+                                    src="https://player.piksel.com/player.php?p=dek7nez2&wmode=transparent&r=true"
                                     height="400" width="640"
                                     allowtransparency="true" allowfullscreen>
                                 </iframe>
